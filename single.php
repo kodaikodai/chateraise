f017c2848cf790c704aaf9b9c98abbae4e18e92e
kodaikodai <k.sagawaa@gmail.com>
2021-01-24 14:57:35 +0900

    商品画面css修正

--- single.php
+++ single.php
@@ -17,14 +17,16 @@
         <p class='sentence'><?php the_content(); ?></p>
         <p>アレルギー：<?php echo get_post_meta($post->ID, 'item_allergies', true); ?></p>
         <form action="<?php echo get_page_link(2215)?>" method="post">
-          <div>
+          <div class="buy-num">
             <span>数量</span>
             <input type="number" value="1"  min="1" name="num">
           </div>
           <div>
             <input type="hidden" name="item_id" value="<?php echo get_the_ID()?>">
             <input type="hidden" name="price" value="<?php echo get_post_meta($post->ID, 'item_price', true);?>">
-            <input type="submit" value="カートに入れる">
+            <div class="add_cart">
+              <input type="submit" value="カートに入れる" class="btn btn-square-shadow" onfocus="this.blur();">
+            </div>
           </div>
         </form>
       </div>
